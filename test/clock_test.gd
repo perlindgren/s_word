bfa725d501fdf777239f96b8c2c5f719fd003924
Per Lindgren <per.lindgren@ltu.se>
2026-08-23 00:24:00 +0200

    time is key

--- test/clock_test.gd
+++ test/clock_test.gd
@@ -5,8 +5,16 @@ extends Node2D
 var sec: int = 0
 
 func _ready() -> void:
-	pass
 	
-func _process(_delta) -> void:
-	sec += 11
-	clock.set_seconds(sec)	
+	#tween.tween_method(
+		#func(s:float):
+			#clock.s = s
+	#, 0.0, 3600.0, 4
+	#).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
+	
+	
+	clock.s = 0
+	for i in range(1, 10):
+		var tween = create_tween()
+		await tween.tween_property(clock,"s", 3600.0 * i, 4).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN_OUT).finished
+	
