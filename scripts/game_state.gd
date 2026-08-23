9715df7caa41d334996a92cbdee4e7c29b6828ee
Per Lindgren <per.lindgren@ltu.se>
2026-08-23 19:25:53 +0200

    game loop wip

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -1,3 +1,36 @@
 extends Node
 
 @export var master_sword_mode = false
+
+# The cleared challenges
+@export var cleared : Array = [0, 2, 5, 8, 11]
+@export var event_nr : int = 2
+
+class Data:
+	var name : String
+	var event : String
+	var word : String
+	var extra_chars : int
+	var flipped : float
+	
+	func _init(n: String, e: String, w: String, ex: int, f: float):
+		name = n
+		event = e
+		word = w
+		extra_chars = ex
+		flipped = f
+
+var events : Array[Data] = [
+	Data.new("Goblin", "Kill", "SWORD", 0, 0),
+	Data.new("Dwarf", "Wants", "GOLD", 1, 0.2),
+	Data.new("Robot", "Feed", "OIL", 2, 0.3),
+	Data.new("House", "Help", "WATER", 3, 0.4),
+	Data.new("Dog", "Feed", "BONE", 4, 0.5),
+	Data.new("Pillow Man", "Wants", "BLANKET", 5, 0.6),
+	Data.new("Seagull with Gun", "Feed", "FRIES", 6, 0.7),
+	Data.new("Cat", "Feed", "CATNIP", 6, 0.8),
+	Data.new("Pizza Man", "Wants", "Tomatoe", 7, 0.9),
+	Data.new("Fish", "Wants", "AQUARIUM", 7, 1.0),
+	Data.new("Cake", "Needs", "BIRTHDAY", 8, 1.0),
+	Data.new("John Smith", "Wants", "SMARTPHONE", 8, 1.0)
+]
--- scripts/signals.gd
+++ scripts/signals.gd
@@ -5,3 +5,7 @@ extends Node
 signal main_menu_start
 signal to_start_menu
 signal to_game_loop
+
+signal new_event
+
+signal dropped(p: int, s: String)
