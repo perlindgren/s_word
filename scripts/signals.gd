d6de177d7a0d47f4ea24e8a69f16984b5582a8fd
Per Lindgren <per.lindgren@ltu.se>
2026-08-24 15:45:55 +0200

    Hourglass and unused signals

--- scripts/signals.gd
+++ scripts/signals.gd
@@ -1,11 +1,11 @@
 extends Node
 
 # Signals
+# Project->GDScript->Unused Signal IGNORE
 
+# Schene transitions
 signal to_start_menu
 signal to_game_loop_menu
 signal to_game_loop
-
-signal new_event
 
 signal dropped(p: int, s: String)
