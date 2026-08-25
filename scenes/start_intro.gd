551fbd5f3eea5e5c650e87d4d9dd02a4cebe1aa6
Per Lindgren <per.lindgren@ltu.se>
2026-08-25 09:42:36 +0200

    intro with bounce

--- scenes/start_intro.gd
+++ scenes/start_intro.gd
@@ -2,16 +2,19 @@ extends Node2D
 
 @onready var n65 = $N65
 @onready var the_pen = $ThePen
+@onready var s_word = $SWORD
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 @export var text_to_show: String = "THE PEN IS MIGHTIER THAN THE"
-@export var n65_duration: float = 1.0
+@export var n65_duration: float = 5.0
 @export var display_speed: float = 0.1 # Time in seconds per character
+@export var s_word_tween_time: float = 3
 
 var last_visible_chars = 0
 
 func _ready() -> void:
 	the_pen.modulate = Color.TRANSPARENT
+	s_word.modulate = Color.TRANSPARENT
 	the_pen.visible_characters = 0
 	last_visible_chars = 0
 	var total_chars = the_pen.get_total_character_count()
@@ -23,7 +26,15 @@ func _ready() -> void:
 	the_pen.modulate = Color.WHITE
 	
 	tween = create_tween()
-	tween.tween_property(the_pen, "visible_characters", total_chars, duration)
+	await tween.tween_property(the_pen, "visible_characters", total_chars, duration).finished
+	
+	s_word.position.y = -165
+	s_word.modulate = Color.WHITE
+	
+	# 524
+	tween = create_tween()
+	await tween.tween_property(s_word, "position:y", 524, s_word_tween_time).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT).finished
+	
 	
 func _process(_delta: float) -> void:
 	# Detect exactly when a new character is revealed on screen
