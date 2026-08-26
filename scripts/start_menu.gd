c7bfefa56d65765c1f3138b01b5ee097e761bbb5
Per Lindgren <per.lindgren@ltu.se>
2026-08-26 21:29:21 +0200

    smooth clock

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -8,6 +8,8 @@ class_name StartMenu extends Node2D
 @onready var start2 = $Start2
 @onready var clock : Clock = $Clock
 
+var tween: Tween = null
+
 func _ready() -> void:
 	set_state()
 	
@@ -45,15 +47,26 @@ func set_state() -> void:
 	else:
 		quick.modulate = Color.WHITE
 		master.modulate = Color(1,1,1, 0.25)
-			
-func _process(_delta) -> void:
+		
+	var to_s = compute_s() + 4 * (1 if GameState.master_sword_mode else 60)
+	tween = create_tween()
+	await tween.tween_method(
+		func(s:float):
+			clock.s = s
+	, clock.s, to_s, 4
+	).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN_OUT).finished	
+	tween = null
+		
+func compute_s() -> float:
 	var local_time_dict = Time.get_datetime_dict_from_system()
 	var s = local_time_dict.hour * 3600 + local_time_dict.minute * 60 + local_time_dict.second
-	if GameState.master_sword_mode:
-		clock.s = s
-	else:
-		clock.s = s * 60
+	s = s if GameState.master_sword_mode else 60 * s
+	return fmod(s, 12 * 3600)
 
+func _process(_delta) -> void:
+	if !tween:
+		clock.s = compute_s()
+		
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		print("_unhandled_input ui_cancel, emit to_game_loop_menue")
